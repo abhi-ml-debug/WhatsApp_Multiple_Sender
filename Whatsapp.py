132e080ee9286f28b940a7850ce6cf4202c01e0a
abhi-ml-debug <58108727+abhi-ml-debug@users.noreply.github.com>
2020-09-03 18:14:20 +0530

    Update Whatsapp.py
    Added comments

--- Whatsapp.py
+++ Whatsapp.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
+
 driver=webdriver.Chrome('./chromedriver.exe')
 char='y'
 contacts=[]
@@ -9,12 +11,17 @@ while char=='y':
  char=input("Want to enter more contacts? (y/n) \n")
 msg=input("Enter the message\n")
 freq=int(input("Enter the frequency\n"))
+
 driver.get("https://web.whatsapp.com")
-import time
 search_bar_path = "//*[@id=\"side\"]/div[1]/div/label/div/div[2]"
 msg_bar_path="//*[@id=\"main\"]/footer/div[1]/div[2]/div/div[2]"
+#The message bar's and search bar's XPath can be obtained 
+#by doing a right click, inspecting element, and selecting "Copy XPath" of the required element  
+
 for contact in contacts:
- driver.implicitly_wait(35)
+ driver.implicitly_wait(40)
+ #Wait till a maximum of 40 seconds (Needed for QR code scan for first time)
+ #Stop waiting as soon as search bar is found 
  element=driver.find_element_by_xpath(search_bar_path)
  element.click()
  time.sleep(2)
@@ -24,8 +31,10 @@ for contact in contacts:
  element=driver.find_element_by_xpath(msg_bar_path)
  element.click()
  time.sleep(2)
+ 
  for i in range(freq):
   element.send_keys(msg)
   element.send_keys(Keys.ENTER)
   time.sleep(1)
+
 driver.quit()
